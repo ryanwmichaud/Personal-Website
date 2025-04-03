eec581dc9b4292240aea153a9fa162b5f2872e31
Ryan Michaud <ryanwmichaud@gmail.com>
2025-04-03 13:55:53 -0500

    bird quiz link

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -5,6 +5,7 @@ import Project from "../components/Project"
 const Projects = ()=>{
 
     const chordFinderDescription = "Web App that finds every possible way to voice a group of chord tones on a string instrument within a maximum number of frets and generates a diagram visualization. Supports any tuning and number of strings. Custom user authentication and optional Google authentication."
+    const birdsQuizDescription = "Web app that allows a user to quiz themselves on a diverse set of bird calls and songs from a chosen set of species."
     const harmonizerDescription = "MIDI plugin for a DAW which allows users to explore constant structure harmony. Harmonizes incoming MIDI notes in real time with all possible inversions of a user provided structure."
     return(
         <div className="background">
@@ -21,6 +22,14 @@ const Projects = ()=>{
                     technologies={"Node.js, React, Express, MYSQL"} 
                     thumbnailLink={"http://chords.ryanwmichaud.com"}
                     titleLink={"https://github.com/ryanwmichaud"}></Project>
+
+                    <Project title={"Bird Song Quiz"} 
+                    thumbnail={"/projects/Birds/bird-thumbnail.png"}  
+                    description={birdsQuizDescription} 
+                    technologies={"Node.js, React, Vite, FastAPI"} 
+                    thumbnailLink={"https://birds.ryanwmichaud.com"}
+                    titleLink={"https://github.com/ryanwmichaud/Birds"}></Project>
+                     
                     
                     <Project title={"Harmonizer Plugin"} 
                     thumbnail={"/projects/Harmonizer/Harmonizer Demo.png"}  
